da7f8ddc8774c52960e51feb070c5674125c7a16
fernandobloedorn <fernandobloedorn@gmail.com>
2020-04-25 11:06:15 -0300

    Initial commit

---
+++ precache-manifest.37fc4a67246910df42782227a3743e45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94a4c8ef016d4b360fdaf8601241040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b3f0fc5830d8c0d9a9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "74b0dead85ce55e81a22",
+    "url": "/static/js/2.9981469b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9981469b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b3f0fc5830d8c0d9a9",
+    "url": "/static/js/main.8e47868c.chunk.js"
+  },
+  {
+    "revision": "c21d47c6a269191f6ad1",
+    "url": "/static/js/runtime-main.197b1636.js"
+  }
+]);
